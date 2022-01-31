daec0fabb5277c01f768d5e723c22c9a667c6207
ShadeDoyle <97891673+ShadeDoyle@users.noreply.github.com>
2022-01-31 21:16:49 +0000

    Forecast real time data

--- src/script.js
+++ src/script.js
@@ -99,25 +99,41 @@ function formatDate(date) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+
+  return days[day];
+}
 function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
-  let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      ` <div class="col-2">
-              <div class="forecast-date">${day}</div>
+  let forecast = response.data.daily;
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        ` <div class="col-2">
+              <div class="forecast-date">${formatDay(forecastDay.dt)}</div>
               <img
-                src="http://openweathermap.org/img/wn/50d@2x.png"
+                src="http://openweathermap.org/img/wn/${
+                  forecastDay.weather[0].icon
+                }@2x.png"
                 width="42"
-                alt=""
+                alt="" 
               />
               <div class="forecast-temp">
-                <span class="forecast-max"> 9˚</span>
-                <span class="forcast-min">6˚</span>
+                <span class="forecast-max"> ${Math.round(
+                  forecastDay.temp.max
+                )} ˚</span>
+                <span class="forcast-min">${Math.round(
+                  forecastDay.temp.min
+                )}  ˚</span>
               </div>
             </div>`;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
